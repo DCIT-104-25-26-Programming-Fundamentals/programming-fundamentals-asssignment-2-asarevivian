1ae5a464b084d37e1f309f79dabe725caad825a2
asarevivian <vaasare023@st.ug.edu.gh>
2026-08-02 15:59:38 -0700

    Complete assignment 1: prime number checker

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,33 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    // Numbers less than 2 are not prime
+    if (n < 2) {
+        return false;
+    }
+
+    // Check for divisors from 2 up to sqrt(n)
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
